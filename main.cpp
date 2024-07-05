3195ab67d7bad764a0252fa4efe4ccd7017dd386
Ilia Platone <info@iliaplatone.com>
2024-07-05 21:45:31 +0200

    Add QML project

--- main.cpp
+++ main.cpp
@@ -1,26 +1,58 @@
-#include "mainwindow.h"
-#include <config.h>
+/*
+   MIT License
 
-#include <QApplication>
+   libahp_xc library to drive the AHP XC correlators
+   Copyright (C) 2020  Ilia Platone
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 #ifdef _WIN32
 #include <windows.h>
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
 {
     int argc = 0;
     char **argv = {NULL};
-    QApplication a(argc, argv);
 #else
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
 #endif
-    MainWindow w;
-    w.setWindowTitle("GT Configurator - Version " GT_CONFIGURATOR_VERSION " Engine " + QString::number(ahp_gt_get_version(), 16));
-    QFont font = w.font();
-    font.setPixelSize(12);
-    w.setFont(font);
-    w.show();
-    a.setWindowIcon(QIcon(":/icons/icon.ico"));
-    return a.exec();
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
 }
--- main.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.13
-import QtQuick.Window 2.13
-
-Window {
-    width: 640
-    height: 480
-    visible: true
-    title: qsTr("Hello World")
-}
